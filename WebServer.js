4494da3a35323eefd82aaccc62c371043c378513
Jia-Jie-Liao <alex0521945@gmail.com>
2025-04-22 21:58:28 +0800

    ejs view engine

--- WebServer.js
+++ WebServer.js
@@ -1,28 +1,5 @@
 const http = require('http')
 const fs = require('fs')
-const dayjs = require('dayjs')
-
-const utc = require('dayjs/plugin/utc')
-const timezone = require('dayjs/plugin/timezone')
-
-const isBetween = require("dayjs/plugin/isBetween")
-
-console.log(dayjs().year())
-console.log(dayjs().hour())
-console.log(dayjs().minute())
-console.log(dayjs().second())
-// Time Zone
-dayjs.extend(utc)
-dayjs.extend(timezone)
-const d1 = dayjs.tz('2025-05-21 12:00', 'America/New_York')
-console.log(d1.toISOString())
-// Is Before
-const d2 = dayjs().isBefore(dayjs('2026-01-01'), 'month')
-console.log(d2.toString())
-// Is Between
-dayjs.extend(isBetween)
-const d3 = dayjs("2016-10-30").isBetween("2016-01-01", "2016-10-30", 'day', "[)")
-console.log(d3.toString())
 
 const server = http.createServer((req,res) => {
     // console.log('訪客請求已收到');
--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 const express = require('express')
+const dayjs = require('dayjs')
 
 const app = express()
 
+app.set('view engine','ejs')
+app.set('views','page')     //express在page裡找回相應的ejs檔案
+
 app.get('/', (req,res) => {
     // res.send('<h1>Home<h1>')
-    res.sendFile('./page/index.html', {root:__dirname})
+    let now = `現在是${dayjs().hour()}時${dayjs().minute()}分${dayjs().second()}秒`
+    res.render('index',{courseName : 'NodeJS入門', time : now})
 })
 
 app.get('/about', (req,res) => {
-    res.sendFile('./page/about.html', {root:__dirname})
+    res.render('about',{courseName : 'NodeJS入門'})
 })
 
 app.get('/aboutme', (req,res) => {
-    res.redirect('/about')
+    res.redirect('about')
 })
 
 app.use((req,res) => {
-    res.status(404).sendFile('./page/404.html', {root:__dirname})
+    res.status(404).render('404')
     // res.status(404)
 })
 
